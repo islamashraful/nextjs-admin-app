c1f21ed299aaf58872160d08d9acd3fa9209b855
Ashraful Islam <rx.ju.edu@gmail.com>
2024-02-11 20:55:53 +0700

    User details

---
+++ app/dashboard/users/[id]/page.jsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+
+import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
+
+const user = {
+  id: "12312",
+  username: "ashraful",
+  email: "ashraful@gmail.com",
+  phone: "12321",
+  address:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, quas!",
+  isAdmin: false,
+  isActive: false,
+};
+
+const SingleUserPage = async ({ params }) => {
+  const { id } = params;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src={"/noavatar.png"} alt="" fill />
+        </div>
+        {"ashraful"}
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="hidden" name="id" value={user.id} />
+          <label>Username</label>
+          <input type="text" name="username" placeholder={user.username} />
+          <label>Email</label>
+          <input type="email" name="email" placeholder={user.email} />
+          <label>Password</label>
+          <input type="password" name="password" />
+          <label>Phone</label>
+          <input type="text" name="phone" placeholder={user.phone} />
+          <label>Address</label>
+          <textarea type="text" name="address" placeholder={user.address} />
+          <label>Is Admin?</label>
+          <select name="isAdmin" id="isAdmin">
+            <option value={true} selected={user.isAdmin}>
+              Yes
+            </option>
+            <option value={false} selected={!user.isAdmin}>
+              No
+            </option>
+          </select>
+          <label>Is Active?</label>
+          <select name="isActive" id="isActive">
+            <option value={true} selected={user.isActive}>
+              Yes
+            </option>
+            <option value={false} selected={!user.isActive}>
+              No
+            </option>
+          </select>
+          <button>Update</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SingleUserPage;
