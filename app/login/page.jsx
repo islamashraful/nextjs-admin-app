f545153b9348cd4d92ce57055ec41fbc9ae1575e
Ashraful Islam <rx.ju.edu@gmail.com>
2024-02-11 21:00:30 +0700

    Login screen

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,13 @@
+import LoginForm from "@/app/ui/login/loginForm/loginForm";
+
+import styles from "@/app/ui/login/login.module.css";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
