31c3b44720ae98fc093300ce5eba549a7be065dd
Ashraful Islam <rx.ju.edu@gmail.com>
2024-02-11 20:39:21 +0700

    Product List, User List

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,5 +1,107 @@
-const Products = () => {
-  return <div>Products</div>;
+import Image from "next/image";
+import Link from "next/link";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+
+import styles from "@/app/ui/dashboard/products/products.module.css";
+
+const products = [
+  {
+    id: 1,
+    title: "Product 1",
+    desc: "Description of Product 1",
+    price: 19.99,
+    createdAt: new Date("2024-02-10"),
+    stock: 10,
+  },
+  {
+    id: 2,
+    title: "Product 2",
+    desc: "Description of Product 2",
+    price: 29.99,
+    createdAt: new Date("2024-02-09"),
+    stock: 15,
+  },
+  {
+    id: 3,
+    title: "Product 3",
+    desc: "Description of Product 3",
+    price: 24.99,
+    createdAt: new Date("2024-02-08"),
+    stock: 5,
+  },
+  {
+    id: 4,
+    title: "Product 4",
+    desc: "Description of Product 4",
+    price: 39.99,
+    createdAt: new Date("2024-02-07"),
+    stock: 20,
+  },
+];
+
+const Products = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <div className={styles.product}>
+                  <Image
+                    src={product.img || "/noproduct.jpg"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.productImage}
+                  />
+                  {product.title}
+                </div>
+              </td>
+              <td>{product.desc}</td>
+              <td>${product.price}</td>
+              <td>{product.createdAt?.toString().slice(4, 16)}</td>
+              <td>{product.stock}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/products/${product.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination count={100} />
+    </div>
+  );
 };
 
 export default Products;
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,5 +1,78 @@
+import Link from "next/link";
+import Image from "next/image";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+
+import styles from "@/app/ui/dashboard/users/users.module.css";
+
+const users = [
+  {
+    email: "john.doe@gmail.com",
+    createdAt: "31.01.2024",
+    isAdmin: false,
+    isActive: false,
+  },
+];
+
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img || "/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "active" : "passive"}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <input type="hidden" name="id" value={user.id} />
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination count={100} />
+    </div>
+  );
 };
 
 export default Users;
