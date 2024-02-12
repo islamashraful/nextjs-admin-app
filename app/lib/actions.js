0386554c75c1578e3c9cc197bd5d35f5cbcbf59d
Ashraful Islam <rx.ju.edu@gmail.com>
2024-02-12 14:58:16 +0700

    Add user with server action

---
+++ app/lib/actions.js
@@ -0,0 +1,36 @@
+"use server";
+
+import bcrypt from "bcrypt";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { connectToDB } from "./utils";
+import { User } from "./models";
+
+export const addUser = async (formData) => {
+  const { username, email, password, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData);
+
+  try {
+    await connectToDB();
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+      isAdmin,
+      isActive,
+    });
+
+    await newUser.save();
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to create user!");
+  }
+  revalidatePath("/dashboard/users");
+  redirect("/dashboard/users");
+};
